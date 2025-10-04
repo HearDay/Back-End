e31edbedd2c4f5376392ddd8db5af73e400f15df
jih023 <jih023@naver.com>
2025-10-04 21:21:24 +0900

    feat: 아이디 중복 확인 API 구현

--- src/main/java/HearDay/spring/domain/user/controller/UserController.java
+++ src/main/java/HearDay/spring/domain/user/controller/UserController.java
@@ -3,13 +3,11 @@ package HearDay.spring.domain.user.controller;
 import HearDay.spring.common.dto.response.CommonApiResponse;
 import HearDay.spring.domain.user.dto.request.UserRequestDto;
 import HearDay.spring.domain.user.service.UserCommandService;
+import HearDay.spring.domain.user.service.UserQueryService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -18,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserCommandService userCommandService;
+    private final UserQueryService userQueryService;
 
     @PostMapping("/")
     @Operation(summary = "회원가입 API", description = "회원가입시 사용하는 API입니다.")
@@ -25,6 +24,15 @@ public class UserController {
             @RequestBody UserRequestDto request
     ) {
         userCommandService.registerUser(request);
-        return ResponseEntity.ok(CommonApiResponse.success("회원가입 성공", null));
+        return ResponseEntity.ok(CommonApiResponse.success("회원가입에 성공했습니다.", null));
+    }
+
+    @GetMapping("/check-id")
+    @Operation(summary = "아이디 중복 확인 API", description = "회원가입시 아이디 중복 확인에 사용하는 API입니다.")
+    public ResponseEntity<CommonApiResponse<Void>> checkUserId(
+            @RequestParam String userLoginId
+    ) {
+        userQueryService.checkId(userLoginId);
+        return ResponseEntity.ok(CommonApiResponse.success("중복된 아이디가 없습니다.", null));
     }
 }
